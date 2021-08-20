39604765f542c435fdc610be79e899254676106b
roman-moisyeyev <roman.moisyeyev@gmail.com>
2021-08-20 16:52:29 +0300

    project-social-network-20210820

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Header = ()=> {
+    return <header className='header'>
+            <img src='https://image.freepik.com/free-psd/3d-lion-logo-mockup_145275-144.jpg'/>
+        </header>
+}
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Profile = ()=> {
+    return <div className = 'content'>
+        <div>
+            <img src='http://parksadventure.com/wp-content/uploads/2017/10/sunsets-header-2074-1024x300-2.jpg'/>
+        </div>
+        <div>
+            avatar + description
+        </div>
+        <div>
+            Posts
+            <div>
+                new posts
+            </div>
+            <div>post1</div>
+            <div>post2</div>
+            <div>post3</div>
+        </div>
+    </div>
+}
+export default Profile;
